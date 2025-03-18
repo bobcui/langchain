aa933039bab63eb8b7bbd07259e16257da2b6ccf
cuibo <bobcui@yeah.net>
2025-03-18 12:04:37 -0700

    a

--- tests/chat.py
+++ tests/chat.py
@@ -1,5 +1,8 @@
 import getpass
 import os
+from langchain.globals import set_verbose
+
+set_verbose(True)
 
 if not os.environ.get("OPENAI_API_KEY"):
     os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key: ")
