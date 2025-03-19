0c07560ec5b28978331a5e06fa40f93f67ac4551
cuibo <bobcui@yeah.net>
2025-03-19 01:08:18 -0700

    0.1

--- tests/langchain-chatbot/src/chatbot.py
+++ tests/langchain-chatbot/src/chatbot.py
@@ -1,18 +1,35 @@
 import os
-from langchain import ChatOpenAI
-from langchain.chains import ConversationChain
+import getpass
+
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
 
 class Chatbot:
-    def __init__(self):
-        self.llm = ChatOpenAI(model="gpt-3.5-turbo")
-        self.conversation = ConversationChain(llm=self.llm)
+    def __init__(self, thread_id:str=None):
+        self.config = {"configurable": {"thread_id": thread_id}}
+        self.model = init_chat_model("gpt-4o-mini", model_provider="openai")
+        self.workflow = StateGraph(state_schema=MessagesState)
+        self.workflow.add_edge(START, "model")
+        self.workflow.add_node("model", self.__call_model)
+        self.memory = MemorySaver()
+        self.app = self.workflow.compile(checkpointer=self.memory)
 
+    def __call_model(self, state: MessagesState):
+        response = self.model.invoke(state["messages"])
+        return {"messages": response}
+    
     def get_response(self, user_input):
-        response = self.conversation.predict(input=user_input)
-        return response
+        input_messages = [HumanMessage(user_input)]
+        output = self.app.invoke({"messages": input_messages}, self.config)
+        return output["messages"][-1].content
 
 def main():
-    chatbot = Chatbot()
+    if not os.environ.get("OPENAI_API_KEY"):
+        os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key: ")
+
+    chatbot = Chatbot("123")
     print("Welcome to the Langchain Chatbot! Type 'exit' to end the conversation.")
     
     while True:
