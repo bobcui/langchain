21e96f2ae74d0718e1a19d524a13db5e5d9f7014
cuibo <bobcui@yeah.net>
2025-03-21 14:48:17 -0700

    templated

--- tests/langchain-chatbot/src/chatbot.py
+++ tests/langchain-chatbot/src/chatbot.py
@@ -5,26 +5,38 @@ from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+
+class UserMessagesState(MessagesState):
+    language: str
 
 class Chatbot:
     def __init__(self, thread_id:str=None):
         self.config = {"configurable": {"thread_id": thread_id}}
         self.model = init_chat_model("gpt-4o-mini", model_provider="openai")
         
-        self.workflow = StateGraph(state_schema=MessagesState)
-        self.workflow.add_edge(START, "model")
+        self.workflow = StateGraph(state_schema=UserMessagesState)
         self.workflow.add_node("model", self.__call_model)
+        self.workflow.add_edge(START, "model")
+
+        self.prompt_template = ChatPromptTemplate.from_messages([(
+                "system", "You talk like a pirate. Answer all questions to the best of your ability in {language}.",
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+        ])
 
         self.memory = MemorySaver()
         self.app = self.workflow.compile(checkpointer=self.memory)
 
-    def __call_model(self, state: MessagesState):
-        response = self.model.invoke(state["messages"])
+    def __call_model(self, state: UserMessagesState):
+        prompt = self.prompt_template.invoke(state)
+        response = self.model.invoke(prompt)
         return {"messages": response}
 
     def get_response(self, user_input):
         input_messages = [HumanMessage(user_input)]
-        output = self.app.invoke({"messages": input_messages}, self.config)
+        output = self.app.invoke({"messages": input_messages, "language": "Chinese"}, self.config)
         return output["messages"][-1].content
 
 
