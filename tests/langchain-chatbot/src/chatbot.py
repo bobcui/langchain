1a2639418bc258aaf28956885c740b384acc4ccb
cuibo <bobcui@yeah.net>
2025-03-21 01:03:25 -0700

    0.2

--- tests/langchain-chatbot/src/chatbot.py
+++ tests/langchain-chatbot/src/chatbot.py
@@ -10,26 +10,29 @@ class Chatbot:
     def __init__(self, thread_id:str=None):
         self.config = {"configurable": {"thread_id": thread_id}}
         self.model = init_chat_model("gpt-4o-mini", model_provider="openai")
+        
         self.workflow = StateGraph(state_schema=MessagesState)
         self.workflow.add_edge(START, "model")
         self.workflow.add_node("model", self.__call_model)
+
         self.memory = MemorySaver()
         self.app = self.workflow.compile(checkpointer=self.memory)
 
     def __call_model(self, state: MessagesState):
         response = self.model.invoke(state["messages"])
         return {"messages": response}
-    
+
     def get_response(self, user_input):
         input_messages = [HumanMessage(user_input)]
         output = self.app.invoke({"messages": input_messages}, self.config)
         return output["messages"][-1].content
 
+
 def main():
     if not os.environ.get("OPENAI_API_KEY"):
         os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key: ")
 
-    chatbot = Chatbot("123")
+    chatbot = Chatbot(thread_id="123")
     print("Welcome to the Langchain Chatbot! Type 'exit' to end the conversation.")
     
     while True:
